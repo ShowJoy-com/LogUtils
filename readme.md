5c760c7045623e4962177872702da22e82aa8c6f
路飞 <lufei@showjoy.com>
2017-05-23 15:13:25 +0800

    统一日志模块

---
+++ readme.md
@@ -0,0 +1,30 @@
+# 统一日志模块
+
+根据不同的buildType，对日志进行不同处理。错误日志可以通过注入的接口，自定义处理，比如上传到监控平台等。
+支持获取当前日志的行数，支持当前日志的堆栈信息。
+
+## LogUtils的使用
+
+#### 1、aar已上传Jcenter，只要如下gradle依赖即可
+
+    compile 'com.showjoy.android:log:1.0.0'
+    
+#### 2、首先在application的onCreate里调用init
+
+    public class MainApplication extends Application {
+        @Override
+        public void onCreate() {
+           super.onCreate();
+
+           LogUtils.TAG = "What you like";
+           LogUtils.LOG_DEBUG = BuildConfig.DEBUG;
+
+           //错误日志监控
+           LogUtils.setErrorListener(...);
+           //一般日志监控
+           LogUtils.setLogListener(...);
+        }
+    }
+
+
+​
---
+++ src/main/java/com/showjoy/android/utils/SHLog.java
@@ -0,0 +1,199 @@
+package com.showjoy.android.utils;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+/**
+ * 所有log 通过这个类打印
+ * Created by lufei on 3/22/16.
+ */
+public class SHLog {
+
+    public static String TAG = "ShowJoy";
+
+    public static boolean LOG_DEBUG = false;
+
+    public static int maxLength = 4000;
+
+    private static final String SUFFIX = ".java";
+
+    static ErrorListener sErrorListener;
+    static LogListener sLogListener;
+
+    public interface ErrorListener {
+        void error(Throwable e);
+        void error(String error);
+    }
+
+    public interface LogListener {
+        void log(Throwable e);
+        void log(String error);
+    }
+
+    public static void setErrorListener(ErrorListener errorListener) {
+        sErrorListener = errorListener;
+    }
+
+    public static void setLogListener(LogListener logListener) {
+        sLogListener = logListener;
+    }
+
+
+    private static String getClassAndMethod() {
+
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+
+        if (null == stackTrace) {
+            return "";
+        }
+
+        StackTraceElement destStackTraceElement = null;
+        boolean next = true;
+        for (StackTraceElement traceElement : stackTrace) {
+            if (traceElement.getClassName().equals(SHLog.class.getName())) {
+                next = false;
+            } else {
+                if (next) {
+                    continue;
+                } else {
+                    destStackTraceElement = traceElement;
+                    break;
+                }
+            }
+        }
+
+        if (null == destStackTraceElement) {
+            return "";
+        }
+
+        String className = destStackTraceElement.getClassName();
+        String[] classNameInfo = className.split("\\.");
+        if (classNameInfo.length > 0) {
+            className = classNameInfo[classNameInfo.length - 1] + SUFFIX;
+        }
+
+        if (className.contains("$")) {
+            className = className.split("\\$")[0] + SUFFIX;
+        }
+
+        String methodName = destStackTraceElement.getMethodName();
+        int lineNumber = destStackTraceElement.getLineNumber();
+
+        if (lineNumber < 0) {
+            lineNumber = 0;
+        }
+
+        String methodNameShort = methodName.substring(0, 1).toUpperCase() + methodName.substring(1);
+
+
+        String headString = "[(" + className + ":" + lineNumber + ")#" + methodNameShort + "]";
+
+        return headString;
+    }
+
+    public static void d(Object... messages) {
+        if (LOG_DEBUG) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (Object msg : messages) {
+                if (null == msg) {
+                    continue;
+                }
+                stringBuilder.append(msg.toString());
+                stringBuilder.append(" ");
+            }
+            if (stringBuilder.length() > 0) {
+                SHLog.d(stringBuilder.toString());
+            }
+        }
+    }
+
+    public static void e(String... messages) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String msg : messages) {
+            stringBuilder.append(msg);
+            stringBuilder.append("\n");
+        }
+
+        if (LOG_DEBUG) {
+            SHLog.e(stringBuilder.toString());
+        }
+        if (null != sErrorListener) {
+            sErrorListener.error(stringBuilder.toString());
+        }
+    }
+
+    //调用原生log接口
+
+    public static void d(String msg) {
+        if (LOG_DEBUG) {
+            if (TextUtils.isEmpty(msg)) {
+                return;
+            }
+
+            String header = getClassAndMethod();
+
+            Log.d(TAG, header + Thread.currentThread().toString() + ":");
+
+            int index = 0;
+            int countOfSub = msg.length() / maxLength;
+
+            if (countOfSub > 0) {
+                for (int i = 0; i < countOfSub; i++) {
+                    String sub = msg.substring(index, index + maxLength);
+                    Log.d(TAG,  sub);
+                    index += maxLength;
+                }
+                Log.d(TAG, msg.substring(index, msg.length()));
+            } else {
+                Log.d(TAG, msg);
+            }
+
+            if (null != sLogListener) {
+                sLogListener.log(Thread.currentThread().toString() + ":");
+                sLogListener.log(header + msg);
+            }
+        }
+    }
+
+    public static void e(String msg) {
+
+        String header = getClassAndMethod();
+
+        if (LOG_DEBUG) {
+            if (TextUtils.isEmpty(msg)) {
+                return;
+            }
+
+            Log.e(TAG, header + Thread.currentThread().toString() + ":");
+
+            int index = 0;
+            int countOfSub = msg.length() / maxLength;
+
+            if (countOfSub > 0) {
+                for (int i = 0; i < countOfSub; i++) {
+                    String sub = msg.substring(index, index + maxLength);
+                    Log.e(TAG,  sub);
+                    index += maxLength;
+                }
+                Log.e(TAG, msg.substring(index, msg.length()));
+            } else {
+                Log.e(TAG, msg);
+            }
+
+        }
+
+        if (null != sErrorListener) {
+            sErrorListener.error(header + msg);
+        }
+    }
+
+    public static void e(Throwable throwable) {
+        if (LOG_DEBUG) {
+            String header = getClassAndMethod();
+            Log.e(TAG, Thread.currentThread().toString() + header, throwable);
+        }
+        if (null != sErrorListener) {
+            sErrorListener.error(throwable);
+        }
+    }
+}
